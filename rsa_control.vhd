df3810def931323aed5d092116b53c191b3e92b4
alvargu <a.guddingsmo@outlook.com>
2024-11-05 15:21:33 +0100

    added port map for template rsa_control

--- rsa_control.vhd
+++ rsa_control.vhd
@@ -5,5 +5,25 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity rsa_control is
      port(
-          
-     )
+          --input controll
+		valid_in	: in std_logic;
+		ready_in	: out std_logic;
+
+		--input data
+		message 	: in STD_LOGIC_VECTOR( C_block_size-1 downto 0 );
+		key 		: in STD_LOGIC_VECTOR( C_block_size-1 downto 0 );
+
+		--ouput controll
+		ready_out	: in std_logic;
+		valid_out	: out std_logic;
+
+		--output data
+		result 	: out STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+
+		--modulus
+		modulus 	: in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+
+		--utility
+		clk 		: in std_logic;
+		reset_n 	: in std_logic
+     );
