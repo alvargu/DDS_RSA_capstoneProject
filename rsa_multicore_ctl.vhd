247e943aa1d9fb2d475c948369f93ba2e4aec3cd
alvargu <92863607+alvargu@users.noreply.github.com>
2024-11-13 15:12:44 +0100

    rmved component instantiation to test code

--- rsa_multicore_ctl.vhd
+++ rsa_multicore_ctl.vhd
@@ -57,7 +57,6 @@ component exponentiation
 		--utility
 		clk 		: in STD_LOGIC;
 		reset_n 	: in STD_LOGIC
-	
 	);
 end component;
 
@@ -65,7 +64,7 @@ end component;
 -------------------------------------------------------------------------------
 -- Internal signal declaration
 -------------------------------------------------------------------------------
-signal valid_in_1 :
+-- signal valid_in_1 :
 
 
 
@@ -73,44 +72,45 @@ begin
 -------------------------------------------------------------------------------
 -- Port mapping to exponentiation modules (rsa modules)
 -------------------------------------------------------------------------------
-module_1 : exponentiation port map (
-	valid_in => valid_in_1,
-	ready_in => ready_in_1,
-	message => message_1,
-	key => key_1,
-	ready_out => ready_out_1,
-	valid_out => valid_out_1,
-	result => result_1,
-	modulus => modulus_1,
-	clk => clk_1,
-     reset_n => reset_n_1,
-);
 
-module_2 : exponentiation port map (
-	valid_in => valid_in_2,
-	ready_in => ready_in_2,
-	message => message_2,
-	key => key_2,
-	ready_out => ready_out_2,
-	valid_out => valid_out_2,
-	result => result_2,
-	modulus => modulus_2,
-	clk => clk_2,
-     reset_n => reset_n_2,
-);
-
-module_3 : exponentiation port map (
-	valid_in => valid_in_3,
-	ready_in => ready_in_3,
-	message => message_3,
-	key => key_3,
-	ready_out => ready_out_3,
-	valid_out => valid_out_3,
-	result => result_3,
-	modulus => modulus_3,
-	clk => clk_3,
-     reset_n => reset_n_3,
-);
+-- module_1 : exponentiation port map (
+-- 	valid_in => valid_in_1,
+-- 	ready_in => ready_in_1,
+-- 	message => message_1,
+-- 	key => key_1,
+-- 	ready_out => ready_out_1,
+-- 	valid_out => valid_out_1,
+-- 	result => result_1,
+-- 	modulus => modulus_1,
+-- 	clk => clk_1,
+--      reset_n => reset_n_1,
+-- );
+-- 
+-- module_2 : exponentiation port map (
+-- 	valid_in => valid_in_2,
+-- 	ready_in => ready_in_2,
+-- 	message => message_2,
+-- 	key => key_2,
+-- 	ready_out => ready_out_2,
+-- 	valid_out => valid_out_2,
+-- 	result => result_2,
+-- 	modulus => modulus_2,
+-- 	clk => clk_2,
+--      reset_n => reset_n_2,
+-- );
+-- 
+-- module_3 : exponentiation port map (
+-- 	valid_in => valid_in_3,
+-- 	ready_in => ready_in_3,
+-- 	message => message_3,
+-- 	key => key_3,
+-- 	ready_out => ready_out_3,
+-- 	valid_out => valid_out_3,
+-- 	result => result_3,
+-- 	modulus => modulus_3,
+-- 	clk => clk_3,
+--      reset_n => reset_n_3,
+-- );
 
 
 
