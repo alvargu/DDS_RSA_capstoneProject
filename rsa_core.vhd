40af9b771707b188876ebb25f661bacaaf8e77bc
Jens636 <jens_636@hotmail.com>
2024-11-21 17:17:46 +0100

    Update rsa_core.vhd

--- rsa_core.vhd
+++ rsa_core.vhd
@@ -1,91 +1,154 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 entity rsa_core is
-     generic(
-          C_block_size : integer := 256;
-          C_CORE_CNT : integer := 15
-     );
-     port(
-        -----------------------------------------------------------------------------
-		-- Clocks and reset
-		-----------------------------------------------------------------------------
+	generic (
+		-- Users to add parameters here
+		C_BLOCK_SIZE          : integer := 256
+	);
+	
+	port (
 		clk                    :  in std_logic;
 		reset_n                :  in std_logic;
-
-		-----------------------------------------------------------------------------
-		-- Slave msgin interface
-		-----------------------------------------------------------------------------
-		-- Message that will be sent out is valid
-		msgin_valid             : inout std_logic;
-		-- Slave ready to accept a new message
-		msgin_ready             : inout std_logic;
-		-- Message that will be sent out of the rsa_msgin module
-		msgin_data              :  inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		-- Indicates boundary of last packet
-		msgin_last              :  inout std_logic;
-
-		-----------------------------------------------------------------------------
-		-- Master msgout interface
-		-----------------------------------------------------------------------------
-		-- Message that will be sent out is valid
-		msgout_valid            : inout std_logic;
-		-- Slave ready to accept a new message
-		msgout_ready            :  inout std_logic;
-		-- Message that will be sent out of the rsa_msgin module
-		msgout_data             : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		-- Indicates boundary of last packet
-		msgout_last             : inout std_logic;
-
-		-----------------------------------------------------------------------------
-		-- Interface to the register block
-		-----------------------------------------------------------------------------
+		msgin_valid             : in std_logic;
+		msgin_ready             : out std_logic;
+		msgin_data              :  in std_logic_vector(C_BLOCK_SIZE-1 downto 0);
+		msgin_last              :  in std_logic;
+		msgout_valid            : out std_logic;
+		msgout_ready            :  in std_logic;
+		msgout_data             : out std_logic_vector(C_BLOCK_SIZE-1 downto 0);
+		msgout_last             : out std_logic;
 		key_e_d                 :  in std_logic_vector(C_BLOCK_SIZE-1 downto 0);
 		key_n                   :  in std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		rsa_status              : inout std_logic_vector(31 downto 0)
-     );
-end entity;
+		rsa_status              : out std_logic_vector(31 downto 0)
+	);
+end rsa_core;
 
 architecture rtl of rsa_core is
-    signal valid: std_logic_vector(C_CORE_CNT-1 downto 0);
+
+	component exponentiation
+		port (
+			start : in std_logic;
+			done : out std_logic;
+	
+			--input data
+			message 	: in STD_LOGIC_VECTOR (C_BLOCK_SIZE-1 downto 0 );
+			key 		: in STD_LOGIC_VECTOR (C_BLOCK_SIZE-1 downto 0 );
+	
+			--output data
+			result 		: out STD_LOGIC_VECTOR(C_BLOCK_SIZE-1 downto 0);
+	
+			--modulus
+			modulus 	: in STD_LOGIC_VECTOR(C_BLOCK_SIZE-1 downto 0);
+	
+			--utility
+			clk 		: in STD_LOGIC;
+			reset_n 	: in STD_LOGIC
+		);
+	end component;
+
+	signal exporesult : std_logic_vector(C_BLOCK_SIZE-1 downto 0);
+	signal expodone : std_logic := '0';
+	signal expo_EN, expo_EN_nxt : std_logic := '0';
+    signal mreg, mreg_nxt : std_logic_vector(C_BLOCK_SIZE-1 downto 0);
+	type state is (IDLE, WAIT_FOR_VALID, LOAD, SEND_READY, EXPO, SEND_VALID, WAIT_FOR_READY);
+    signal curr_state, next_state : state;
+    --signal validin, validin_nxt : std_logic := '0';
+    --signal readyout, readyout_nxt : std_logic := '0';
+    --signal validout_nxt : std_logic:= '0';
+    --signal readyin_nxt : std_logic:= '0';
 begin
-    core_gen: for core_nr in 0 to C_CORE_CNT-1 generate --14 for max
-        i_exponentiation : entity work.exponentiation
-            generic map (
-                C_block_size => C_BLOCK_SIZE
-		    )
-		    port map (
-             	valid_in        => msgin_valid,
-             	ready_in        => msgin_ready,
-               	message         => msgin_data,
-             	key             => key_e_d,
-             	ready_out       => msgout_ready,
-             	valid_out       => msgout_valid,
-             	result          => msgout_data,
-             	modulus         => key_n,
-             	clk             => clk,
-                reset_n         => reset_n
-            );
-    end generate;
-    /*
-    RSA_INPUT_HANDLER: entity work.rsa_input_handler
-        generic map (
-            C_BLOCK_SIZE => C_BLOCK_SIZE
-        )
-        port map (
-            clk => clk,
-            reset_n => reset_n
-        );
-    
-    RSA_OUTPUT_HANDLER: entity work.rsa_output_handler
-        generic map (
-            C_BLOCK_SIZE => C_BLOCK_SIZE
-        )
-        port map (
-            clk => clk,
-            reset_n => reset_n
-        );
-    */
-end architecture;
+		expos : exponentiation port map ( start => expo_EN_nxt,
+								done => expodone,
+								message => mreg,
+								key => key_e_d,
+								result => exporesult,
+								modulus => key_n,
+								clk => clk,
+								reset_n => reset_n);
+	rsa_status   <= (others => '0');
+
+	process(clk, reset_n)
+    begin
+        if (reset_n = '0') then
+            curr_state <= WAIT_FOR_VALID;
+        else 
+            if (clk'event and clk = '1') then
+                curr_state <= next_state;
+            end if;
+        end if;
+    end process;
+
+
+	process(clk, reset_n)
+    begin
+        if (reset_n = '0') then
+            mreg <= (others => '0');
+            --validin <= '0';
+            --readyout <= '0';
+            expo_EN <= '0';
+        else
+            if (clk'event and clk = '1') then
+                mreg <= mreg_nxt;
+                --validin <= validin_nxt;
+                --readyout <= readyout_nxt;    
+                expo_EN <= expo_EN_nxt;
+            end if;
+        end if;
+    end process;
+
+
+
+process(all)
+begin
+    next_state <= curr_state;
+    expo_EN_nxt <= expo_EN;
+    mreg_nxt <= mreg;
+    case(curr_state) is
+        when IDLE => 
+            msgin_ready <= '0';
+            msgout_valid <= '0';
+            next_state <= WAIT_FOR_VALID;
+        when WAIT_FOR_VALID =>
+            if (msgin_valid = '1') then
+                next_state <= LOAD;
+            else
+                next_state <= WAIT_FOR_VALID;
+            end if;
+        when LOAD => 
+            mreg_nxt <= msgin_data;
+            next_state <= SEND_READY;
+        when SEND_READY => 
+            msgin_ready <= '1';
+            msgout_last <= msgin_last;
+            next_state <= EXPO;
+        when EXPO => 
+            msgin_ready <= '0';
+            expo_EN_nxt <= '1';
+            if (expodone = '1') then
+                next_state <= SEND_VALID;
+                expo_EN_nxt <= '0';
+            else
+                next_state <= EXPO;
+            end if;
+        when SEND_VALID => 
+            msgout_valid <= '1';
+            if (msgout_ready = '1') then
+                msgout_data <= exporesult;
+                next_state <= IDLE;
+            else
+                next_state <= WAIT_FOR_READY;
+            end if;
+        when WAIT_FOR_READY => 
+            if (msgout_ready = '1') then
+                msgout_data <= exporesult;
+                next_state <= IDLE;
+            else
+                next_state <= WAIT_FOR_READY;
+            end if;
+        when others => 
+            next_state <= IDLE;
+    end case;
+end process;
+
+end rtl;
