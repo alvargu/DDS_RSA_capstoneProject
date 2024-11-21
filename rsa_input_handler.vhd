16d4fae4ae6db62661d7635a2d561fea62136c70
alvargu <a.guddingsmo@outlook.com>
2024-11-21 18:00:12 +0100

    cleanup of inputhandler (still no communication with output handler)

--- rsa_input_handler.vhd
+++ rsa_input_handler.vhd
@@ -3,15 +3,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+--#############################################################################
+-- Entity I/O Definition
+--#############################################################################
 entity rsa_input_handler is
      generic(
           C_block_size : integer := 256;
-          C_status_size : integer := 32;
+		-- Not using status register ATM
+          --C_status_size : integer := 32;
           C_msg_id_size : integer := 4;
           C_CORE_CNT : integer := 15
      );
      port(
-        -----------------------------------------------------------------------------
+        	-----------------------------------------------------------------------------
 		-- Clocks and reset
 		-----------------------------------------------------------------------------
 		clk                    :  in std_logic;
@@ -36,9 +40,9 @@ entity rsa_input_handler is
           -- Indicates boundary of last packet
 		h_msgin_last          : out std_logic;
 		-- input
-		core_id_recieved	    : in std_logic;
+		h_core_id_recieved	    : in std_logic;
 		-- Signal indicating to output handler a core has been started
-     	core_id_sent		    : out std_logic;
+     	h_core_id_sent		    : out std_logic;
 
 		-----------------------------------------------------------------------------
 		-- Core interface
@@ -68,16 +72,15 @@ end entity;
 -- Functional code (not tested) (without com to out handler)
 -- ######################################
 architecture rtl of rsa_input_handler is
-    -------------------------------------------------------------------------------
-    -- Internal signal declaration
-    -------------------------------------------------------------------------------
-    -- should not be nessecary with internal signal for core id: 
-    signal internal_core_id : std_logic_vector(C_msg_id_size-1 downto 0) := (others => '0');
-	-- Used for several diff things. 
-	signal curr_core_bm : std_logic_vector(C_CORE_CNT-1 downto 0) := '1' & (others => '0');
+    	--########################################################################
+    	-- Internal signal declaration
+    	--########################################################################
+    	-- should not be nessecary with internal signal for core id but is used for saftey: 
+    	signal internal_core_id : std_logic_vector(C_msg_id_size-1 downto 0) := (others => '0');
+	-- 
 	signal curr_core_ready : std_logic := '0';
 
-	signal save_core_id_recieved : std_logic := '0';
+	-- signal save_h_core_id_recieved : std_logic := '0';
 
 	signal msgin_data_register : std_logic_vector := (others => '0');
 
@@ -85,9 +88,9 @@ architecture rtl of rsa_input_handler is
 	type state is (IDLE, CORE_FOUND, LOAD_CORE, OUT_H_COM);
 	signal curr_state, next_state : state;
 
----------------------------------------------------------------------
+--#############################################################################
 -- Begin architecture
----------------------------------------------------------------------
+--#############################################################################
 begin
 
 ---------------------------------------------------------------------
@@ -101,12 +104,17 @@ il_msgout_data <= msgin_data_register;
 -- Throughput signals to output handler
 ---------------------------------------------------------------------
 h_msgin_last <= msgin_last;
+h_core_id <= internal_core_id;
 
----------------------------------------------------------------------
+--#############################################################################
 -- Proces for handling actions in each state
----------------------------------------------------------------------
+--#############################################################################
 p_core_handler : process (curr_state)
 begin
+	-- Sett default values for msgin_ready and il_msgin_valid
+	msgin_ready <= '0';
+	il_msgin_valid <= (others => '0');
+	-- Defining what actions to take in each state
 	case (curr_state) is
 		when IDLE =>
                --
@@ -130,46 +138,46 @@ begin
 			il_msgin_valid(to_integer(internal_core_id)) <= '1';
 
 		when OUT_H_COM =>
-			-- no longer loading into core when sending
-			msgin_ready <= '0';
-			il_msgin_valid <= (others => '0');
-          when others => 
-			msgin_ready <= '0';
-               il_msgin_valid <= (others => '0');
+			-- 
+
+          when others => null;
      end case;
 end process;
 
 
----------------------------------------------------------------------
+--#############################################################################
 -- State machine for core handling states
----------------------------------------------------------------------
-sm_core_handler : process (all)
+--#############################################################################
+sm_core_handler : process 	(curr_state, 
+						curr_core_ready, 
+						msgin_valid, 
+						il_msgin_ready, 
+						h_core_id, 
+						h_core_id_recieved)
 begin
+	-- default to same state as
+	next_state <= curr_state;
 	case (curr_state) is
 		when IDLE =>
+			-- 
                if ((curr_core_ready = '1')) then
 			    	next_state <= CORE_FOUND;
-               else
-               	next_state <= IDLE;
                end if;
-          when CORE_FOUND => 
+          when CORE_FOUND =>
+			-- 
                if ((msgin_valid = '1')) then
                    	next_state <= LOAD_CORE;
 				msgin_data_register <= msgin_data;
-               else
-                   next_state <= CORE_FOUND;
                end if;
-		when LOAD_CORE => 
+		when LOAD_CORE =>
+			-- 
 			if (il_msgin_ready(to_integer(h_core_id)) = '0') then
                    next_state <= OUT_H_COM;
-               else
-                   next_state <= LOAD_CORE;
                end if;
-		when OUT_H_COM => 
-			if (core_id_recieved = '1') then
+		when OUT_H_COM =>
+			-- 
+			if (h_core_id_recieved = '1') then
                    next_state <= IDLE;
-               else
-                   next_state <= OUT_H_COM;
                end if;
           when others =>
                next_state <= IDLE;
@@ -177,9 +185,9 @@ begin
 end process;
 
 
--------------------------------------------------------------------------------
+--#############################################################################
 -- Update state on rising edge
--------------------------------------------------------------------------------
+--#############################################################################
 process(clk, reset_n)
 begin
     	if (reset_n = '0') then
