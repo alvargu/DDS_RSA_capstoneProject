dcb963b7a49fe1f2255c7751432743b5ee684f15
alvargu <a.guddingsmo@outlook.com>
2024-11-22 02:04:10 +0100

    fix to core address variable and semi done commenting

--- rsa_input_handler.vhd
+++ rsa_input_handler.vhd
@@ -64,8 +64,6 @@ architecture rtl of rsa_input_handler is
     	-- Internal signal declaration
     	--########################################################################
 	-- Signals for internal core logic:
-    	-- internal_core_id should not be nessecary with internal signal for core id but is used for saftey: 
-    	signal internal_core_id : std_logic_vector(C_CORE_ID_SIZE-1 downto 0) := (others => '0');
 	-- #############################################################################
 	-- This line might be necessary (if so uncomment il_msgout_data and msgin_data_register)
 	--signal msgin_data_register : std_logic_vector(C_BLOCK_SIZE-1 downto 0) := (others => '0');
@@ -87,48 +85,54 @@ sm_core_handler : process 	(clk,
 						reset_n,
 						fsm_state,
 						msgin_valid,
-						il_msgin_ready,
-						internal_core_id)
+						il_msgin_ready)
+	variable addr : integer range 0 to C_CORE_CNT-1 := 0;
 begin
      if (clk'event and clk = '1') then
 		if (reset_n = '0') then
      	   	fsm_state <= IDLE;
-			internal_core_id <= (0 => '1', others => '0');
+			addr := 0;
 			msgin_ready <= '0';
 			il_msgin_valid <= (others => '0');
     		else 
 			-- Set default values
 			fsm_state <= fsm_state;
-			internal_core_id <= internal_core_id;
+			addr := addr;
 			msgin_ready <= '0';
 			il_msgin_valid <= (others => '0');
 			case (fsm_state) is
 				--###################################################################
 				when IDLE =>
-					-- 
-     		          if (il_msgin_ready(to_integer(unsigned(internal_core_id))) = '1') then
+					-- Wait for current core to be done
+     		          if (il_msgin_ready(addr) = '1') then
+						-- 
 					    	fsm_state <= CORE_RDY;
      		          end if;
      		     --###################################################################
 				when CORE_RDY =>
 					-- 
 					msgin_ready <= '1';
-					--il_msgin_valid <= (others => '0');
      		          if ((msgin_valid = '1')) then
      		              	fsm_state <= LOAD_CORE;
-						il_msgin_valid(to_integer(unsigned(internal_core_id))) <= '1';
+						il_msgin_valid(addr) <= '1';
 						-- msgin_data_register <= msgin_data;
      		          end if;
 				--###################################################################
 				when LOAD_CORE =>
-					-- 
-					if (to_integer(unsigned(internal_core_id)) >= C_CORE_CNT) then
-						internal_core_id <= (0 => '1', others => '0');
-					elsif (il_msgin_ready(to_integer(unsigned(internal_core_id))) = '0') then
-     		              fsm_state <= IDLE;
-					    internal_core_id <= std_logic_vector(unsigned(internal_core_id) + 1);
+					-- Check if address of core is equal to or larger than max possible
+					
+					-- If address within amount of cores check if current core is done loading in data
+					if (il_msgin_ready(addr) = '0') then
+     		             	-- Go to IDLE state next and increase
+					    	fsm_state <= IDLE;
+						-- Check if address has reached last core and loop back to start if true
+					    	if (addr >= C_CORE_CNT-1) then
+							addr := 0; -- Reset address to zero
+					    	else
+							addr := addr + 1; -- go to next core address
+					    	end if;
 					else
-						il_msgin_valid(to_integer(unsigned(internal_core_id))) <= '1';
+						il_msgin_valid(addr) <= '1';
      		          end if;
 				--###################################################################
 				when others =>
